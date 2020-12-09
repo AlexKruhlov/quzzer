7b0b509d847e215b7bcddbb13799a84a5754c937
Aleksandr Kruhlov <alkruglov777@gmail.com>
2020-12-09 11:02:55 +0200

    Create back-end project with the first settings

---
+++ src/test/java/ua/com/rafael/quizzer/QuizzerApplicationTests.java
@@ -0,0 +1,12 @@
+package ua.com.rafael.quizzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
